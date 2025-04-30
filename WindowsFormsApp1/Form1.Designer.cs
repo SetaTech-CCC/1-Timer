1454c699ed7e8aafaa60468768ab845171d3ed8f
asawasa6 <106855948+asawasa6@users.noreply.github.com>
2025-04-30 17:03:31 +0900

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.startButton.Location = new System.Drawing.Point(12, 105);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(123, 50);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.resetButton.Location = new System.Drawing.Point(160, 105);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(113, 50);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 67);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 167);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch;
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //経過時間を表示
+            TimeSpan ts = stopwatch.Elapsed;
+            label1.Text = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (stopwatch.IsRunning)
+            {
+                stopwatch.Stop();
+                timer.Stop();
+                startButton.Text = "Start";
+            }
+            else
+            {
+                stopwatch.Start();
+                timer.Start();
+                startButton.Text = "Stop";
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+            label1.Text = "00:00:00";
+            startButton.Text = "Start";
+        }
+    }
+}
